3dcb46232cade33424de495c36e8694a000bee28
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2023-07-10 17:30:04 +0700

    add and write liquiubase for bookservice

--- bookservice/src/main/java/com/example/bookservice/command/data/Book.java
+++ bookservice/src/main/java/com/example/bookservice/command/data/Book.java
@@ -3,7 +3,9 @@ package com.example.bookservice.command.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -15,9 +17,11 @@ import javax.persistence.Table;
 @NoArgsConstructor
 public class Book {
     @Id
+    @Column(name = "book_id")
     private String bookId;
     private String name;
     private String author;
+    @Column(name = "is_ready")
     private Boolean isReady;
 
 
